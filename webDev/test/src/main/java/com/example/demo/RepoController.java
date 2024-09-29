5cbb053379722638a49fecac6b68a8aaa1a8aa50
Zi0LEO <leonardo.npl.02@gmail.com>
2024-09-29 20:42:31 +0200

    fixed code, changed directory name

--- webDev/test/src/main/java/com/example/demo/RepoController.java
+++ webDev/test/src/main/java/com/example/demo/RepoController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(path="/test")
 public class RepoController {
 	
+  @Autowired
 	private ContactsRepository repo;
 	
 	
